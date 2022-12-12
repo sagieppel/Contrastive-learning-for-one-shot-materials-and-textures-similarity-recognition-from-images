c26e914b8d9a72e9f000455d39bd66dc879d1104
sagieppel <sagieppel@gmail.com>
2022-12-12 06:28:06 -0500

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,3 +1,6 @@
+# Run train in loop from terminal to in case of crush, restart the script after crash (by defult the script should start from last saved point)
+# You can use this if your program tend to crash once every few thousand steps (OOM) and you dont want to reduce batch size
+
 while true
 do
    echo "Restart training"
@@ -5,6 +8,3 @@ do
    echo "Crushed"
 done
 
-
-# Run in loop to in case of crush (by defult the script should start from last saved point)
-# You can use this if your program tend to crash once every few thousand steps (OOM) and you dont want to reduce batch size
